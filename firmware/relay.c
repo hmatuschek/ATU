46d489826949cb188b6c78d25c3e27fd805c7820
Hannes Matuschek <hmatuschek@gmail.com>
2022-04-03 22:09:41 +0200

    Fixed minior issues.

--- firmware/relay.c
+++ firmware/relay.c
@@ -31,19 +31,31 @@ relay_init() {
 void
 relay_set(Relay relay) {
   if (relay & RELAY_C1) PORT_C1 |= (1<<BIT_C1);
+  else PORT_C1 &= ~(1<<BIT_C1);
   if (relay & RELAY_C2) PORT_C2 |= (1<<BIT_C2);
+  else PORT_C2 &= ~(1<<BIT_C2);
   if (relay & RELAY_C3) PORT_C3 |= (1<<BIT_C3);
+  else PORT_C3 &= ~(1<<BIT_C3);
   if (relay & RELAY_C4) PORT_C4 |= (1<<BIT_C4);
+  else PORT_C4 &= ~(1<<BIT_C4);
   if (relay & RELAY_C5) PORT_C5 |= (1<<BIT_C5);
+  else PORT_C5 &= ~(1<<BIT_C5);
 
   if (relay & RELAY_L1) PORT_L1 |= (1<<BIT_L1);
+  else PORT_L1 &= ~(1<<BIT_L1);
   if (relay & RELAY_L2) PORT_L2 |= (1<<BIT_L2);
+  else PORT_L2 &= ~(1<<BIT_L2);
   if (relay & RELAY_L3) PORT_L3 |= (1<<BIT_L3);
+  else PORT_L3 &= ~(1<<BIT_L3);
   if (relay & RELAY_L4) PORT_L4 |= (1<<BIT_L4);
+  else PORT_L4 &= ~(1<<BIT_L4);
   if (relay & RELAY_L5) PORT_L5 |= (1<<BIT_L5);
+  else PORT_L5 &= ~(1<<BIT_L5);
   if (relay & RELAY_L6) PORT_L6 |= (1<<BIT_L6);
+  else PORT_L6 &= ~(1<<BIT_L6);
 
   if (relay & RELAY_IO) PORT_IO |= (1<<BIT_IO);
+  else PORT_IO &= ~(1<<BIT_IO);
 }
 
 
